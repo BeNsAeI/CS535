4c0885f3e6f509a52c4614b35eba44c399493538
Tekbots <Tekbots>
2019-02-17 11:04:36 -0800

    assignment 2 updated

--- Assignment 2/Instructions.py
+++ Assignment 2/Instructions.py
@@ -4,10 +4,10 @@
 # dict["train_labels"]: a 10000 x 1 vector with each row being the label of one training image, label 0 is an airplane, label 1 is a ship.
 # dict["test_data"]: a 2000 x 3072 matrix with each row being a testing image
 # dict["test_labels]: a 2000 x 1 vector with each row being the label of one testing image, corresponding to test_data.
-#import cPickle
-import pickle
-#dict = cPickle.load(open("cifar_2class_py2.p","rb"))
-dict = pickle.load(open("cifar_2class_py2.p","rb"))
+import cPickle
+#import pickle
+dict = cPickle.load(open("cifar_2class_py2.p","rb"))
+#dict = pickle.load(open("cifar_2class_py2.p","rb"))
 
 for i in dict:
     print (i, dict[i].shape)
